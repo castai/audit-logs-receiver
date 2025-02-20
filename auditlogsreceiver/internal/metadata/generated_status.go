9060bdf23b5f43c3873d95aecfc032b98f8b4fd1
Prasanna Moon <prasannamoon@DarkKnightCloud.local>
2025-02-20 14:37:05 +0530

    datadog example added

--- auditlogsreceiver/internal/metadata/generated_status.go
+++ auditlogsreceiver/internal/metadata/generated_status.go
@@ -4,13 +4,22 @@ package metadata
 
 import (
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/trace"
 )
 
 var (
-	Type      = component.MustNewType("castai_audit_logs")
-	ScopeName = "github.com/castai/audit-logs-receiver/audit-logs"
+	Type = component.MustNewType("castai_audit_logs")
 )
 
 const (
 	LogsStability = component.StabilityLevelAlpha
 )
+
+func Meter(settings component.TelemetrySettings) metric.Meter {
+	return settings.MeterProvider.Meter("otelcol/audit-logs-receiver/auditlogsreceiver")
+}
+
+func Tracer(settings component.TelemetrySettings) trace.Tracer {
+	return settings.TracerProvider.Tracer("otelcol/audit-logs-receiver/auditlogsreceiver")
+}
